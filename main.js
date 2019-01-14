35c5eec21d00058c0e41ad52975ccca5dfd819a2
maxjamin <maxjamin@gmail.com>
2019-01-14 11:40:21 -0700

    Add more

--- main.js
+++ main.js
@@ -11,20 +11,28 @@ function User(name, score, age, ipaddress)
   this.userAge = age;
   this.userIp = ipaddress;
 
+  //display user information
+  this.display = function() {
+  console.log(this.userName + " with the ip address: " + this.userIp 
+  + " has a score of: " + this.userScore + "\n"); }
+  
+  //changes users name
+  this.setName = function(newName) {
+  	this.userName = newName;
+  	console.log(this.userName);
+  }
 }
-
-User.prototype.display = function() {
-  return this.userName + " with the ip address: " + this.userIp 
-  + " has a score of: " + this.userScore + "\n";
-};
-
 
 
 function main()
 {
- var firstUser = new User("Jason", 34, 29, "192.168.1.64");
- var display = firstUser.display();
- console.log(display);
+  var firstUser = new User("Jason", 34, 29, "192.168.1.64");
+  firstUser.display();
+  firstUser.setName("Ben");
+
+  //
+
+
 }
 
 
